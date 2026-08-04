dd320664055862e28e0b918149c62bfd66f8db11
dsharrington84 <dsharrington84@gmail.com>
2026-08-03 19:11:27 -0700

    Add project model exports

---
+++ src/construction_intelligence_mcp/models/__init__.py
@@ -0,0 +1,5 @@
+"""Business models for Construction Intelligence Services."""
+
+from .project import ProjectDetail, ProjectSearchRequest, ProjectSummary
+
+__all__ = ["ProjectDetail", "ProjectSearchRequest", "ProjectSummary"]
